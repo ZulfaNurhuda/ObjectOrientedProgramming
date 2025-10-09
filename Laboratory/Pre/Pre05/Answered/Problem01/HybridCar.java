8447f59ae199b672567641cd437378893881c5c4
Muhammad Zulfa Fauzan Nurhuda <77572803+ZulfaNurhuda@users.noreply.github.com>
2025-10-09 23:44:43 +0700

    fix: Adjust comments for clarity in HybridCar class

--- Laboratory/Pre/Pre05/Answered/Problem01/HybridCar.java
+++ Laboratory/Pre/Pre05/Answered/Problem01/HybridCar.java
@@ -1,8 +1,8 @@
 public class HybridCar extends Hybrid {
     /********************************************************************
-     * Tujuan file ini adalah sebagai "ALIASES" untuk "class Hybrid" *
+     * Tujuan file ini adalah sebagai "ALIASES" untuk "class Hybrid"    *
      * dikarenakan Makefile pada grader membutuhkan file "Hybrid.java", *
-     * tapi Main file pada grader membutuhkan "class HybridCar" *
+     * tapi Main file pada grader membutuhkan "class HybridCar"         *
      ********************************************************************/
     public HybridCar(String brand, String model, int speed, int fuelLevel, int batteryLevelHybrid) {
         super(brand, model, speed, fuelLevel, batteryLevelHybrid);
